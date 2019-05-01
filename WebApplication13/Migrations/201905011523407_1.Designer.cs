e67d7b950a0299e687d6abfc2aaf70eefbdf442f
truong <48508041+TruongTa@users.noreply.github.com>
2019-05-01 22:33:46 +0700

    update Hoàn Thiện file

--- WebApplication13/Migrations/201905011523407_1.Designer.cs
+++ WebApplication13/Migrations/201905011523407_1.Designer.cs
@@ -7,13 +7,13 @@ namespace WebApplication13.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class PopulateLoaiSPTable : IMigrationMetadata
+    public sealed partial class _1 : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(PopulateLoaiSPTable));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(_1));
         
         string IMigrationMetadata.Id
         {
-            get { return "201904280732354_PopulateLoaiSPTable"; }
+            get { return "201905011523407_1"; }
         }
         
         string IMigrationMetadata.Source
