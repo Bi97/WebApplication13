e6bf0637c0070693540e271a10018bdb817f574d
truong <48508041+TruongTa@users.noreply.github.com>
2019-05-04 15:44:44 +0700

    Cập Nhật Layout Đăng Ký - Đăng Nhập

--- WebApplication13/App_Start/IdentityConfig.cs
+++ WebApplication13/App_Start/IdentityConfig.cs
@@ -75,7 +75,7 @@ namespace WebApplication13
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
                 RequireLowercase = true,
                 RequireUppercase = true,
