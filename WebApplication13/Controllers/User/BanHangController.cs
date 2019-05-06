e3cae6da190a54828976d9178f7fe8cc44c088ea
truong <48508041+TruongTa@users.noreply.github.com>
2019-05-06 21:30:08 +0700

    cập nhật form giao diện bán Hàng +SP

--- WebApplication13/Controllers/User/BanHangController.cs
+++ WebApplication13/Controllers/User/BanHangController.cs
@@ -1,21 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication13.Models;
 
 namespace WebApplication13.Controllers
 {
     public class BanHangController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         // GET: BanHang
         public ActionResult BanHang()
-        {
+        { 
             return View();
         }
-        public ActionResult index()
+        /* public ActionResult index()
+         {
+             return View();
+         }*/
+
+        //tìm kiếm
+
+        public ActionResult Index()
         {
+           
             return View();
         }
+
     }
 }
