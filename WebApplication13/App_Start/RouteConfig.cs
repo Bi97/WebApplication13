e7bde7c8a71efb3c547e758859fb31ef7ec8c7c1
Phi Long <knplong@gmail.com>
2019-06-20 19:57:25 +0700

    z

--- WebApplication13/App_Start/RouteConfig.cs
+++ WebApplication13/App_Start/RouteConfig.cs
@@ -11,47 +11,8 @@ namespace WebApplication13
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");                   
-            //------------------------------Sản Phẩm---------------------------//
-            routes.MapRoute(
-              name: "SanPham",
-              url: "San-Pham/{Action}",
-              defaults: new { controller = "SanPhams", action = "Index", id = UrlParameter.Optional }
-          );
-            //=====================================Giới Thiệu=============================================================//
-            routes.MapRoute(
-                name: "GioiThieu",
-                url: "Trang-Gioi-Thieu",
-                defaults: new { controller = "GioiThieu", action = "Index", id = UrlParameter.Optional }
-            );
-            //------------------------------default---------------------//
-
-            routes.MapRoute(
-                name: "TrangChuAdmin",
-                url: "Admin/{action}",
-                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
-            );
-
-            //default
-            routes.MapRoute(
-                name: "TrangChuAdmin",
-                url: "Admin/{action}",
-                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
-            );
-
-            //default
-            routes.MapRoute(
-                name: "TrangChuAdmin",
-                url: "Admin/{action}",
-                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
-            );
-
-            //default
-            routes.MapRoute(
-                name: "TrangChuAdmin",
-                url: "Admin/{action}",
-                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
-            );
+            
+            
 
             //default
             routes.MapRoute(
